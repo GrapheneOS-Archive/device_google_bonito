c102f5db5af35b87e94ac9c4ebcf834311226d0d
Daniel Micay <danielmicay@gmail.com>
2021-07-15 21:06:38 -0400

    set PRODUCT_BRAND to hardware brand

--- aosp_sargo.mk
+++ aosp_sargo.mk
@@ -61,7 +61,7 @@ ifneq (REL,$(PLATFORM_VERSION_CODENAME))
 endif
 
 PRODUCT_MANUFACTURER := Google
-PRODUCT_BRAND := Android
+PRODUCT_BRAND := google
 PRODUCT_NAME := aosp_sargo
 PRODUCT_DEVICE := sargo
 PRODUCT_MODEL := Pixel 3a
