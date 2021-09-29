268c4740606af7248a4112b957d23fcd3568d9a6
flawedworld <flawedworld@flawed.world>
2021-09-29 04:34:42 +0100

    Use APNs and CarrierConfig from APV

--- bonito.mk
+++ bonito.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/bonito/aosp_bonito.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := bonito
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
