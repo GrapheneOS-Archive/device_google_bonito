a5cd877977d0ca4bae03a890fd5b0108c23ac1fe
flawedworld <flawedworld@flawed.world>
2021-09-29 04:34:42 +0100

    Use APNs and CarrierConfig from APV

--- sargo.mk
+++ sargo.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/bonito/aosp_sargo.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := sargo
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
